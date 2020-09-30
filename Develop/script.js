d0e649b8cb0281cef7e1a0d7afe2544cdd88ea8c
Andrew Elliott <andrewjc.elliott@gmail.com>
2020-09-30 18:57:24 -0400

    Updated notes for each line of Javascript

--- Develop/script.js
+++ Develop/script.js
@@ -11,18 +11,25 @@ var numbers = "1234567890".split("");
 // special character options
 var specialCharacters = "!@#$%^&*()_+".split("");
 
-// prompts for each character option 
+// questions for password options
+// prompt for Character length 
 var characterLength = prompt("Choose number of password charaters between 8 and 128.");
+// if number chosen is not between 8 and 128 return error alert 
 if (characterLength < 8 || characterLength > 128) {
   alert("You must choose between 8 and 128. Please try again.")
 }
+// if number is between 8 and 128 ask additional questions
 else {
+  // confirm lowercase characters 
   var confirmLowercase = confirm("Would you like to include lowercase characters?");
+  // confirm uppercase characters 
   var confirmUppercase = confirm("Would you like to include uppercase characters?");
+  // confirm numbers 
   var confirmNumbers = confirm("Would you like to include numbers?");
+  // confirm special characters 
   var confirmSpecialCharacters = confirm("Would you like to include special characters?");
 };
-
+// if no option is true return error alert 
 if (confirmLowercase && confirmUppercase && confirmNumbers && confirmSpecialCharacters === false) {
   alert("You must say yes to at least one option! Please try again.")
 }
