b72e70e3a73c048378f5770d650bb18b20490486
Andrew Elliott <andrewjc.elliott@gmail.com>
2020-09-30 18:53:19 -0400

    Reordered sequence of events. Initiated prompts and confirms with if/else conditional statements

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,31 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// button to activate prompts
+// options for lowercase, uppercase, numeric, special characters
+// lowercase options
+var lowercase = "abcdefghijklmnopqrstuvwxyz".split("");
+// uppercase options 
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+// numeric optiions 
+var numbers = "1234567890".split("");
+// special character options
+var specialCharacters = "!@#$%^&*()_+".split("");
+
 // prompts for each character option 
-var characterLength = prompt("How many characters would you like your password to be?");
-var confirmLowercase = confirm("Would you like to include lowercase characters?");
-var confirmUppercase = confirm("Would you like to include uppercase characters?");
-var confirmNumbers = confirm("Would you like to include numbers?");
-var confirmSpecialCharacters = confirm("Would you like to include special characters?");
+var characterLength = prompt("Choose number of password charaters between 8 and 128.");
+if (characterLength < 8 || characterLength > 128) {
+  alert("You must choose between 8 and 128. Please try again.")
+}
+else {
+  var confirmLowercase = confirm("Would you like to include lowercase characters?");
+  var confirmUppercase = confirm("Would you like to include uppercase characters?");
+  var confirmNumbers = confirm("Would you like to include numbers?");
+  var confirmSpecialCharacters = confirm("Would you like to include special characters?");
+};
+
+if (confirmLowercase && confirmUppercase && confirmNumbers && confirmSpecialCharacters === false) {
+  alert("You must say yes to at least one option! Please try again.")
+}
 
 console.log(characterLength);
 console.log(confirmLowercase);
@@ -15,15 +33,6 @@ console.log(confirmUppercase);
 console.log(confirmNumbers);
 console.log(confirmSpecialCharacters);
 
-// options for lowercase, uppercase, numeric, special characters
-// lowercase options
-var lowercase = "abcdefghijklmnopqrstuvwxyz";
-// uppercase options 
-var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-// numeric optiions 
-var numbers = "1234567890"
-// special character options
-var specialCharacters = "!@#$%^&*()_+"
 
 // validate input with AT LEAST one character type seelcted
 // if at least one character type selected 
