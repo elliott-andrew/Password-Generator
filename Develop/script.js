544087691639be8a8bf3db6901e2c2694af6374a
Andrew Elliott <andrewjc.elliott@gmail.com>
2020-09-30 16:05:56 -0400

    Added pseudocode outline

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,19 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// button to activate prompts
+// options for each password
+// choose length of the password <= 128 characters
+// options for lowercase, uppercase, numeric, special characters
+// lowercase options
+// uppercase options 
+// numeric optiions 
+// special character options 
+// validate input with AT LEAST one character type seelcted
+// password generated to match criteria
+// generated password then displayed on page via alert or message 
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
